42d45c4f4cdd90fc13f5b8a406a0f463bf304455
Ivan Goncharov <i.morph@gmail.com>
2019-06-04 07:55:08 +0300

    CHANGE: time printed using NTP

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/imorph/ntp-now
 
 go 1.12
 
-require github.com/imorph/ntp-now/pkg/myntp v0.0.4
+require (
+	github.com/imorph/ntp-now/pkg/myntp v0.0.5
+	github.com/stretchr/testify v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	myntp "github.com/imorph/ntp-now/pkg/myntp"
 )
 
 func main() {
-	fmt.Println("Current time is: ", myntp.Now())
+	ntpTime, err := myntp.Now()
+
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Anyway, here is your LOCAL time: ", ntpTime)
+		os.Exit(1)
+	}
+	
+	fmt.Println("Current NETWORK time according to time.apple.com is: ", ntpTime)
 }
